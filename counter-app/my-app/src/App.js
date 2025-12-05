6907ca33329248696c6a01f895ec140361070f92
Urja Rathod <118295018+Urjarathod28@users.noreply.github.com>
2025-12-05 10:17:15 +0530

    Add files via upload

---
+++ counter-app/my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Countapp from './increment_decrement';
+function App() {
+  return (
+    <div className="App">
+      <Countapp/>
+    </div>
+  );
+}
+
+export default App;
